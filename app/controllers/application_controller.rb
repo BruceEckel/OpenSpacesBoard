9cf37a6e7627df85e527dc7cc9c808dbda8e38db
Jeremy Cerise <jcerise06@gmail.com>
2013-12-20 15:27:53 -0700

    Adding comments to new code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # Returns the current user (if there is one), as defined by the user session object
+  # This session object is created when a user logs in via OAuth
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  # Get the url for the currently logged in users gravatar image (if they have one)
+  # Some providers (Github) provide an image URL, which is stored upon user creation. If a url is present on the
+  # user, use that, otherwise, generate a gravatar url from the user email
   def avatar_url(user)
     if (user.image_url == 'none')
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,30 +2,44 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # Check authorizations and create a new user if none are found, otherwise, log the user in based on their
+  # existing authorization
   def create
+    # Grab an authorizatio token from the provider selected
     auth_hash = request.env['omniauth.auth']
 
+    # Check if there is an auth token stored that matches the returned token
+    # If there is a match, the user alread exists, if not, they need to be created
     @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     if @authorization
+      # Create the user session object
       session[:user_id] = @authorization.user_id
       redirect_to root_url, :notice => "Signed in!"
     else
+      # Create a new user
+
+      # Check if the auth token contains an email address, and set accordingly
+      # Not all providers return email
       if auth_hash["info"]["email"] == nil
         email = 'none'
       else
         email = auth_hash["info"]["email"]
       end
 
+      # Check if the auth token has an image url for the user, and set accordingly
+      # Not all providers return this information
       if auth_hash["info"]["image"] != nil
         image = auth_hash["info"]["image"]
       else
         image = "none"
       end
 
+      # Create the new user, adding the authorization information to the database for later user
       user = User.new :name => auth_hash["info"]["name"], :email => email, :image_url => image
       user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
       user.save
 
+      # Create the user session object
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Signed in!"
     end
@@ -34,6 +48,7 @@ class SessionsController < ApplicationController
   def failure
   end
 
+  # Destroy the user session object, logging the user out
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "You have logged out!"
